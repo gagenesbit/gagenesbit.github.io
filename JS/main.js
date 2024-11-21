5a61b36edda7a844a1ce265301eb3a16b62445dd
Gage Nesbit <gagenesbit31@gmail.com>
2024-11-21 10:33:14 -0700

    Fixed Some Spelling Errors

--- JS/main.js
+++ JS/main.js
@@ -1,8 +1,8 @@
 var webDescription= "Proficiency in HTML, CSS, JavaScript and additional experience with PHP and HTMX. With a specialization in hypermedia systems and the advantages that they provide."
 
-var DBDescription="I have experience working with relational and non-relational database technologies, SQL, and database design. I have worked with databases using python and java in a variety of contexts."
+var DBDescription="I have experience working with relational and non-relational database technologies, SQL, and database design. I have worked with databases using Python and Java in a variety of contexts."
 
-var securityDescription="I have experience investigating and learning how cybersecurity vulnerabilities are exploited. Then I took that knowlege and implemented security measures in my own projects. I am currently working as a graduate students reasearch assistant in the MSU Cybersecurity lab."
+var securityDescription="I have experience investigating and learning how cybersecurity vulnerabilities are exploited. Then I took that knowledge and implemented security measures in my own projects. I am currently working as a graduate student research assistant in the MSU Cybersecurity lab."
 
 var gitDescritpion="Highly proficient using Git and GitHub to manage versions of a codebase. This allows for a heavily collaborative workflow in which my teams were able to deliver quality software before deadlines."
 function changeDescription(option) {
